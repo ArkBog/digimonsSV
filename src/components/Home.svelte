44ec4678fc18a5c301fa3f0939fa9084c42dd5bd
ArkBog <arkadiuszbogdanski1@gmail.com>
2024-01-30 20:27:22 +0100

    added filter function

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,10 +1,11 @@
 <script lang>
-// @ts-nocheck
+  // @ts-nocheck
 
   import { digimons, favourites } from "./stores";
-  import {goto} from "$app/navigation";
+  import { goto } from "$app/navigation";
 
   let digimonsToDisplay = [];
+  
 
   const getData = fetch("https://digimon-api.vercel.app/api/digimon")
     .then((response) => response.json())
@@ -14,56 +15,81 @@
     })
     .catch((error) => console.error(error));
 
-    const addToFavourites = (digimon) => {
-      if($favourites.includes(digimon)){
-        let index = $favourites.findIndex((e)=> e === digimon);
-        $favourites.splice(index, 1)
-      }
-      else{
-        $favourites.push(digimon);
-      }
-      console.log($favourites) 
+  const addToFavourites = (digimon) => {
+    if ($favourites.includes(digimon)) {
+      let index = $favourites.findIndex((e) => e === digimon);
+      $favourites.splice(index, 1);
+    } else {
+      $favourites.push(digimon);
     }
+    console.log($favourites);
+  };
 
-  const navigateToComponent = () => goto('/details');
+  const navigateToComponent = () => goto("/details");
 
   const filters = () => {
     let filtersArray = [];
-    for(let i = 0; i < $digimons.length; i++){
-      if(filtersArray.includes($digimons[i].level) && filtersArray.length != 0){
-        continue
-      }
-      else{
-        filtersArray.push($digimons[i].level)
+    for (let i = 0; i < $digimons.length; i++) {
+      if (
+        filtersArray.includes($digimons[i].level) &&
+        filtersArray.length != 0
+      ) {
+        continue;
+      } else {
+        filtersArray.push($digimons[i].level);
       }
     }
-    return filtersArray
-  }
+    return filtersArray;
+  };
 
-  let runFilter = (...arg)=>{
-    console.log(arg)
-    return digimonsToDisplay =  digimonsToDisplay.filter((e)=>arg.includes(e.level)) ;
-  }
+  let choosenFilters = [];
 
+  let runFilter = (arg) => {
+      if (choosenFilters.includes(arg)) {
+        let index = choosenFilters.findIndex((e) => e === arg);
+        choosenFilters.splice(index, 1);
+        console.log(choosenFilters)
+      } else {
+        choosenFilters.push(arg);
+        console.log(choosenFilters)
+      }
+        digimonsToDisplay = $digimons.filter((e) =>
+        choosenFilters.includes(e.level)
+      );
+      if(choosenFilters.length === 0){
+        digimonsToDisplay = $digimons
+      }
+  };
 </script>
 
 <div class="container">
-  
   {#await getData}
     Data is loading
   {:then data}
-  <div class="filters">
-    {#each filters() as filter }
-      <input type="checkbox" name="filter" value={filter} on:change={(event)=>runFilter(event.target.value)}>
-      <label for="filter">{filter}</label>
-    {/each}
-  </div>
+    <div class="filters">
+      {#each filters() as filter}
+        <input
+          type="checkbox"
+          name="filter"
+          value={filter}
+          on:change={(event) => runFilter(event.target.value)}
+        />
+        <label for="filter">{filter}</label>
+      {/each}
+    </div>
     {#each digimonsToDisplay as digimon}
-      <div class="card" >
-        <h1 class="card-name" >{digimon.name}</h1>
+      <div class="card">
+        <h1 class="card-name">{digimon.name}</h1>
         <div class="card-img">
-          <img class="digimon-img" src={digimon.img} alt={digimon.name} on:click={() => navigateToComponent()}/>
-          <button class="add-to-fav" on:click={() => addToFavourites(digimon)}>Add to favourites</button>
+          <img
+            class="digimon-img"
+            src={digimon.img}
+            alt={digimon.name}
+            on:click={() => navigateToComponent()}
+          />
+          <button class="add-to-fav" on:click={() => addToFavourites(digimon)}
+            >Add to favourites</button
+          >
         </div>
         <p class="card-text">{digimon.level}</p>
       </div>
@@ -79,30 +105,31 @@
     display: flex;
     flex-direction: column;
   }
-  .card{
+  .card {
     border-radius: 2rem;
     width: 300px;
     margin: 1rem auto;
     background-color: var(--secondary-color);
     overflow: hidden;
   }
-  h1, .card-text{
+  h1,
+  .card-text {
     margin: auto;
     text-align: center;
     color: var(--text-color);
     font-weight: 600;
     padding: 5px 0;
   }
-  .card-img{
+  .card-img {
     position: relative;
     overflow: hidden;
   }
-  .digimon-img:hover{
+  .digimon-img:hover {
     transition: var(--transition-details);
     transform: scale(1.2);
     cursor: pointer;
   }
-  .add-to-fav{
+  .add-to-fav {
     position: absolute;
     top: 0px;
     right: 0px;
